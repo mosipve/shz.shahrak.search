d314231ca34a669f14a416dce8660a17f73633bb
mosi <mosipvp@gmail.com>
2016-12-11 18:12:16 +0330

    Add files via upload

---
+++ inc/_conn.php
@@ -0,0 +1,24 @@
+﻿<?php
+//load database connection
+    $host = "localhost";		//HOST 
+    $user = "root";				//USER_NAME
+    $password = "mosi";			//PASSWORD
+    $database_name = "serch";	//DATABASE_NAME
+    $pdo = new PDO("mysql:host=$host;dbname=$database_name", $user, $password, array(
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ));
+	
+?>
+
+<?php
+// $linkers is your address of pics and KOROKi
+	$linkers = "http://127.0.0.1/MY%20PROJECT/SEARCH/_shahrak_sanati_tempale/company/";
+	$homeLink = "http://127.0.0.1/MY%20PROJECT/SEARCH/_shahrak_sanati_tempale/"; // not set anywhere yet LOL
+	
+// all
+	$siteTtle = "Mosi Search Engine . V: 4.0.0.1";
+	$siteDscription = "This is the Private Web Application for SHAHRAK SANATI SHIRAZ.";
+	$siteKeywrd = "ttouch.biz,sharak,shiraz";
+	$siteAthor = "www.mositech.com";
+	
+?>
---
+++ inc/conn.php
@@ -0,0 +1,45 @@
+﻿
+ <?php
+$servername = "localhost";
+$username = "root";
+$password = "mosi";
+$dbname = "serch";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+/* $sql = "SELECT id, firstname, lastname FROM MyGuests";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+mysqli_close($conn); */
+?> 
+
+
+
+
+
+<?php
+// $linkers is your address of pics and KOROKi
+	$linkers = "http://127.0.0.1/Presentation_shahrak_new/search_engine/company/";
+	$homeLink = "http://127.0.0.1/MY%20PROJECT/SEARCH/_shahrak_sanati_tempale/"; // not set anywhere yet LOL
+	
+// all
+	$siteTtle = "Mosi Search Engine . V: 4.0.0.1";
+	$siteDscription = "This is the Private Web Application for SHAHRAK SANATI SHIRAZ.";
+	$siteKeywrd = "ttouch.biz,sharak,shiraz";
+	$siteAthor = "www.mositech.com";
+	
+?>
